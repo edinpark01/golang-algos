c3f9730dc0ba8f892d36346ed04f6609b1f94cef
Melvin Almonte <melvinalmonte@gmail.com>
2019-08-07 22:28:35 -0400

    Added Node class, started to build linked-list class

--- ma/linked-lists/linkedlist.py
+++ ma/linked-lists/linkedlist.py
@@ -1,1 +1,26 @@
 # TODO: Basic implementation of a linked list and common operations.
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def add(self, data):
+        temp = Node(data)
+        temp.next(self.head)
+        self.head = temp
